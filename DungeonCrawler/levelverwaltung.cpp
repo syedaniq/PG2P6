ca37f287d68516519f2944161b814709d8281595
mofayyad <fayyad.mohammed16@gmail.com>
2022-07-18 17:34:48 +0200

    levelverwaltung an

--- DungeonCrawler/levelverwaltung.cpp
+++ DungeonCrawler/levelverwaltung.cpp
@@ -1,11 +1,18 @@
 #include "levelverwaltung.h"
 #include <fstream>
 #include <cstdlib>
-
+#include <iostream>
 Levelverwaltung::Levelverwaltung(string &dateiname) : dateiname(dateiname)
 {
 }
 
 void Levelverwaltung::einlesen()
 {
+    ifstream quelle(dateiname);
+         if (!quelle.good())
+         {
+           cerr << dateiname << " kann nicht geoeffnet werden!\n";
+           exit(-1);
+         }
+         quelle.close();
 }
